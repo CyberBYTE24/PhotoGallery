d1512c573709da01e6f20eae4a26bfdb208475d8
vershinsky <yvvershinsky@rosatom.ru>
2024-10-22 17:32:14 +0700

    Add descriptions for PhotoGalleryDataService.kt

--- app/src/main/java/com/cyberbyte/photogallery/service/PhotoGalleryDataService.kt
+++ app/src/main/java/com/cyberbyte/photogallery/service/PhotoGalleryDataService.kt
@@ -2,9 +2,13 @@ package com.cyberbyte.photogallery.service
 
 import com.cyberbyte.photogallery.model.Photo
 
+// Provide data managing for app
 class PhotoGalleryDataService(
+    // Retrofit implemented interface for remote REST-API
     private val photoGalleryApiService: PhotoGalleryApiService
 ) {
+
+    // Get photos from remote REST-API
     suspend fun fetchPhotos(): List<Photo>{
         return photoGalleryApiService.getPhotos().photos
     }
