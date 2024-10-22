d226658aaaa3214b01882666bd50f6c1d51a54b6
vershinsky <yvvershinsky@rosatom.ru>
2024-10-22 18:44:23 +0700

    ViewModel completed

--- app/src/main/java/com/cyberbyte/photogallery/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/cyberbyte/photogallery/ui/viewmodel/MainViewModel.kt
@@ -8,12 +8,17 @@ import com.cyberbyte.photogallery.model.Photo
 import com.cyberbyte.photogallery.usecase.GetRemotePhotoGalleryUseCase
 import kotlinx.coroutines.launch
 
+// View Model for Main Activity
 class MainViewModel(
+    // Use case for Getting photos and metadata from remote REST-API
     private val getRemotePhotoGalleryUseCase: GetRemotePhotoGalleryUseCase
 ) : ViewModel() {
+
+    // List of photos
     private val _photos = MutableLiveData<MutableList<Photo>>()
     val photos: LiveData<MutableList<Photo>> get() = _photos
 
+    // Start getting photos from remote REST-API by UseCase
     fun loadPhotos(){
         viewModelScope.launch {
             _photos.value = getRemotePhotoGalleryUseCase.invoke().toMutableList()
