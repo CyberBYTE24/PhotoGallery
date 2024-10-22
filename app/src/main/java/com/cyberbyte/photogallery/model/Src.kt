1948793104c7428306eee6997b814402284de96f
vershinsky <yvvershinsky@rosatom.ru>
2024-10-22 17:18:47 +0700

    Add description for data class Src and it's members

--- app/src/main/java/com/cyberbyte/photogallery/model/Src.kt
+++ app/src/main/java/com/cyberbyte/photogallery/model/Src.kt
@@ -3,7 +3,10 @@ package com.cyberbyte.photogallery.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+// Class represent collection of photo URL's
 data class Src(
+
+    // Original size photo URL
     @SerializedName("original")
     @Expose
     val original: String?
