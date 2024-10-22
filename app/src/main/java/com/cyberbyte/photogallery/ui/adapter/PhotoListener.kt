21d36d21398fa06c7cd4f93050d7a7b4db1f73db
CyberBYTE24 <52069176+CyberBYTE24@users.noreply.github.com>
2024-10-22 18:16:31 +0700

    Update PhotoListener.kt

--- app/src/main/java/com/cyberbyte/photogallery/ui/adapter/PhotoListener.kt
+++ app/src/main/java/com/cyberbyte/photogallery/ui/adapter/PhotoListener.kt
@@ -4,5 +4,7 @@ import com.cyberbyte.photogallery.model.Photo
 
 // Provide handle events of photo items
 interface PhotoListener {
+    
+    // Handle on photo click event
     fun onPhotoClicked(photo: Photo)
 }
